acb411a0072b82f01b6384f3bd6e3a0ccc542216
VanderSant <76136276+VanderSant@users.noreply.github.com>
2021-02-05 21:25:59 -0300

    v1 entregável/one page

---
+++ VandersonSantos/Entregável/JS/httprequest.js
@@ -0,0 +1,23 @@
+
+
+var link = 'https://api.polijunior.com.br/produtos';
+const sendHttpRequest = (method, url, data) =>{
+    const promise = new Promise(( resolve, reject) =>{
+        const xhr= new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.respondeType='json';
+        if(data){
+            xhr.setRequestHeader('Content-Type','application/json');
+        }
+        xhr.onload = () => {
+            resolve(xhr.response);
+        };
+        xhr.onerror = () => {
+            reject('Something went wrong!')
+        }
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+};
+
+export default sendHttpRequest;
---
+++ VandersonSantos/Entregável/JS/index.js
@@ -0,0 +1,81 @@
+var link = 'https://api.polijunior.com.br/produtos';
+
+const sendHttpRequest = (method, url, data) =>{
+    const promise = new Promise(( resolve, reject) =>{
+        const xhr= new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.respondeType='json';
+        if(data){
+            xhr.setRequestHeader('Content-Type','application/json');
+        }
+        xhr.onload = () => {
+            resolve(xhr.response);
+        };
+        xhr.onerror = () => {
+            reject('Something went wrong!')
+        }
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+};
+
+const getData = () =>{
+
+    sendHttpRequest('GET',link)
+    .then(responseData=>{
+        var data = JSON.parse(responseData);
+        var numcapsulas = 4;
+        console.log(data);
+            for(var i = 0;i<numcapsulas;i++){
+                buildcoffeepost(data[i],"coluna-capsulas um");
+                i+=1;
+                if(i>=numcapsulas){break;}
+                buildcoffeepost(data[i],"coluna-capsulas dois");
+                i+=1;
+                if(i>=numcapsulas){break;}
+                buildcoffeepost(data[i],"coluna-capsulas três");
+                i+=1;
+                if(i>=numcapsulas){break;}
+                buildcoffeepost(data[i],"coluna-capsulas quatro");
+            }
+    });
+};
+
+
+function buildcoffeepost(cap, local){
+    var capsula = document.getElementById(local);
+    console.log(cap.nome);
+    var nome = document.createElement("DIV");
+    var inte = document.createElement("DIV");
+    var prec = document.createElement("DIV");
+    var desc = document.createElement("DIV");
+    var div0 = document.createElement("DIV");
+    var div1 = document.createElement("DIV");
+    var div2 = document.createElement("DIV");
+    var img = document.createElement("IMG");
+
+    div0.classList.add("caps0");
+    div1.classList.add("caps1");
+    div2.classList.add("caps2");
+    nome.classList.add("nomecaps");
+    inte.classList.add("intecaps");
+    prec.classList.add("preccaps");
+    desc.classList.add("desccaps");
+    img.classList.add("imgcaps");
+
+    img.src= cap.foto;
+    nome.innerHTML = "<br>"+cap.nome+"<br><br>";
+    inte.innerHTML = "Intensidade: "+cap.intensidade+ "<br><br>";
+    prec.innerHTML = "R$ "+cap.preco+"<br><br>";
+    desc.innerHTML =  cap.descricao+"<br><br>";
+    var aux0 = capsula.appendChild(div0);
+    var aux1 = aux0.appendChild(div1);
+    aux1.appendChild(img);
+    var aux2 = aux0.appendChild(div2);
+    aux2.appendChild(nome);
+    /*aux2.appendChild(desc);
+    aux2.appendChild(inte);
+    aux2.appendChild(prec); */
+};
+
+getData();
